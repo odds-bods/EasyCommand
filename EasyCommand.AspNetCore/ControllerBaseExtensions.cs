b1780154fd02d8d636f5ccb5da9b7bde9858c3c9
Alex Shannon <alexs1305@gmail.com>
2018-09-19 14:26:43 +0100

    refactor(ControllerBaseExtensions): add finding and running tasks before commands

--- EasyCommand.AspNetCore/ControllerBaseExtensions.cs
+++ EasyCommand.AspNetCore/ControllerBaseExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 
@@ -15,28 +15,32 @@ namespace EasyCommand.AspNetCore
 
         public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this object that, IAsyncAspCommand<TRequest, TResult> command)
         {
-            if (that is ControllerBase)
-            {
-                command.SetController((ControllerBase)that);
-            }
+            await ExecuteBeforeCommand(); 
 
             return await command.ExecuteExternalAsync(default(TRequest));
         }
 
         public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this object that, IAsyncAspCommand<TRequest, TResult> command, TRequest request)
         {
-            if (that is ControllerBase)
-            {
-                command.SetController((ControllerBase)that);
-            }
+            await ExecuteBeforeCommand();
 
             return await command.ExecuteExternalAsync(request);
         }
 
-        public static TCommand Command<TCommand>(this object that)
-            where TCommand : IAsyncCommand
+        public static TCommand Command<TCommand>(this object that) where TCommand : IAsyncCommand
         {
             return Scope.ServiceProvider.GetService<TCommand>();
+        }
+
+        private static async Task ExecuteBeforeCommand()
+        {
+            var allExecutingBefore = Scope.ServiceProvider.GetServices<IRunBeforeCommand>()?.ToArray() ?? new IRunBeforeCommand[0];
+
+            if (allExecutingBefore.Any())
+            {
+                await Task.WhenAll(allExecutingBefore.Select(executingBefore =>
+                    executingBefore.BeforeExecutionOfCommand()));
+            }
         }
     }
 }
