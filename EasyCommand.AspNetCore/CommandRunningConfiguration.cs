b46297ca8431631a9cee1cbfac199ce8434da9fe
Alex Shannon <alexs1305@gmail.com>
2018-09-19 14:23:07 +0100

    feat(CommandRunningConfiguration): add config item to store custom config

---
+++ EasyCommand.AspNetCore/CommandRunningConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyCommand.AspNetCore
+{
+    public class CommandRunningConfiguration
+    {
+        internal List<Type> toRunBeforeCommands { get; set; }
+
+        public CommandRunningConfiguration()
+        {
+            toRunBeforeCommands = new List<Type>();
+        }
+
+        public void RunBeforeCommand<T>() where T : IRunBeforeCommand
+        {
+            toRunBeforeCommands.Add(typeof(T));
+        }
+    }
+}
