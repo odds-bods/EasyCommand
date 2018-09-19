c01695b6e615ad84250f3ef75b144bd2e70ed301
Alex Shannon <alexs1305@gmail.com>
2018-09-19 16:50:02 +0100

    refactor(IAsyncAspCommandExtensions): extract aspcommand specific extensions

---
+++ EasyCommand.AspNetCore/IAsyncAspCommandExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace EasyCommand.AspNetCore
+{
+    public static class IAsyncAspCommandExtensions
+    {
+        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this IAsyncAspCommand<TRequest, TResult> command, IAsyncAspCommand<TRequest, TResult> injectedCommand)
+        {
+            return await injectedCommand.ExecuteExternalAsync(default(TRequest));
+        }
+
+        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this IAsyncAspCommand<TRequest, TResult> command, IAsyncAspCommand<TRequest, TResult> injectedCommand, TRequest request)
+        { 
+            return await injectedCommand.ExecuteExternalAsync(request);
+        }
+
+    }
+}
