304ca53cf66541ad3f7bc87ad167175d550416cb
Alex Shannon <alexs1305@gmail.com>
2018-09-24 10:25:54 +0100

    refactor(ControllerBaseExtensions): add execution through handler

--- EasyCommand.AspNetCore/ControllerBaseExtensions.cs
+++ EasyCommand.AspNetCore/ControllerBaseExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace EasyCommand.AspNetCore
 {
@@ -14,43 +15,119 @@ namespace EasyCommand.AspNetCore
             Scope = scope;
         }
 
-        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this object that, IAsyncAspCommand<TRequest, TResult> command)
+        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this ControllerBase that, IAsyncAspCommand<TRequest, TResult> command)
         {
             if (that is ControllerBase)
             {
                 command.SetController((ControllerBase)that);
             }
 
-            await ExecuteBeforeCommand(); 
+            if (CommandRunningConfiguration.Handlers.Length == 0) return await command.ExecuteExternalAsync(default(TRequest));
 
-            return await command.ExecuteExternalAsync(default(TRequest));
+            return await ExecuteThroughHandlers(command, default(TRequest));
         }
 
-        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this object that, IAsyncAspCommand<TRequest, TResult> command, TRequest request)
+        public static async Task<TResult> ExecuteAsync<TRequest, TResult>(this ControllerBase that, IAsyncAspCommand<TRequest, TResult> command, TRequest request)
         {
             if (that is ControllerBase)
             {
                 command.SetController((ControllerBase)that);
             }
 
-            await ExecuteBeforeCommand();
+            if (CommandRunningConfiguration.Handlers.Length == 0) return await command.ExecuteExternalAsync(request);
 
-            return await command.ExecuteExternalAsync(request);
+            return await ExecuteThroughHandlers(command, request);
         }
 
-        public static TCommand Command<TCommand>(this object that) where TCommand : IAsyncCommand
+        public static TCommand Command<TCommand>(this ControllerBase that) where TCommand : IAsyncCommand
         {
             return Scope.ServiceProvider.GetService<TCommand>();
         }
 
-        private static async Task ExecuteBeforeCommand()
-        {
-            var allExecutingBefore = Scope.ServiceProvider.GetServices<IRunBeforeCommand>()?.ToArray() ?? new IRunBeforeCommand[0];
 
-            if (allExecutingBefore.Any())
+        private static async Task<TResult> ExecuteThroughHandlers<TRequest, TResult>(IAsyncAspCommand<TRequest, TResult> command, TRequest request)
+        {
+            var result = default(TResult);
+            var handlerIndex = 0;
+            var commandType = command.GetType();
+
+            var handlers = RetrieveOrderedHandlers();
+
+            await RunHandlersBeforeMethods<IAsyncAspCommand<TRequest, TResult>, TRequest, TResult>(
+                handlers, commandType, request, handlerIndex);
+
+            try
             {
-                await Task.WhenAll(allExecutingBefore.Select(executingBefore =>
-                    executingBefore.BeforeExecutionOfCommand()));
+                result = await command.ExecuteExternalAsync(request);
+            }
+            catch (Exception e)
+            {
+                var message = $"Failed to run  Execute on {handlers[handlerIndex].GetType()}";
+                await RunHandlersAfterMethods<TResult>
+                          (handlers, commandType, result, handlerIndex, message);
+                throw new Exception(message, e);
+            }
+            finally
+            {
+                await RunHandlersAfterMethods<TResult>
+                    (handlers, commandType, result, handlerIndex);
+            }
+
+            return result;
+        }
+
+        private static IAsyncAspCommandHandler[] RetrieveOrderedHandlers()
+        {
+            var handlers = new IAsyncAspCommandHandler[CommandRunningConfiguration.Handlers.Length];
+
+            for (var i = 0; i < CommandRunningConfiguration.Handlers.Length; i++)
+            {
+                handlers[i] = (IAsyncAspCommandHandler)Scope.ServiceProvider.GetService(CommandRunningConfiguration.Handlers[i]);
+            }
+
+            return handlers;
+        }
+
+        private static async Task RunHandlersBeforeMethods<TCommand, TRequest, TResult>(IAsyncAspCommandHandler[] handlers, Type command, TRequest request,
+                int handlerIndex)
+        {
+            for (var i = 0; i < handlers.Length; i++)
+            {
+                handlerIndex = i;
+                try
+                {
+                    await handlers[i].BeforeExecutionAsync(command, request);
+                }
+                catch (Exception e)
+                {
+                    var message = $"Failed to run  BeforeExecutionAsync on {handlers[i].GetType()}";
+
+                    var result = default(TResult);
+
+                    await RunHandlersAfterMethods<TResult>
+                           (handlers, command, result, handlerIndex, message);
+                    throw new Exception(message, e);
+                }
+            }
+        }
+
+        private static async Task RunHandlersAfterMethods<TResult>(IAsyncAspCommandHandler[] handlers, Type command, TResult result,
+                int handlerIndex, string additionalFailureInformation = null)
+        {
+            var hasAdditionalInformation = !(additionalFailureInformation is null);
+
+            for (var i = handlerIndex; i >= 0; i--)
+            {
+                // try catch around each of these
+                // try catch around each of these
+                try
+                {
+                    await handlers[i].AfterExecutionAsync<TResult>(command, result);
+                }
+                catch (Exception e)
+                {
+                    new Exception($"Unable to execute AfterExecutionAsync on {handlers[i].GetType()} {(hasAdditionalInformation ? $"after {additionalFailureInformation}" : null)}", e);
+                }
             }
         }
     }
