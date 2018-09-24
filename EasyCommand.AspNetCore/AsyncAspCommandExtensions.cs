35be25337741104e5e65cadfcc636d176f91b9db
Alex Shannon <alexs1305@gmail.com>
2018-09-24 10:22:22 +0100

    feat(AsyncAspCommandExtensions): rename to show not an interface

---
+++ EasyCommand.AspNetCore/AsyncAspCommandExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace EasyCommand.AspNetCore
+{
+    public static class AsyncAspCommandExtensions
+    {
+        private static IServiceScope Scope;
+
+        public static void RegisterScope(IServiceScope scope)
+        {
+            Scope = scope;
+        }
+
+        public static async Task<TInjectedResult> ExecuteAsync<TRequest, TResult, TInjectedRequest, TInjectedResult>
+            (this IAsyncAspCommand<TRequest, TResult> command, IAsyncAspCommand<TInjectedRequest, TInjectedResult> injectedCommand)
+        {
+            return await injectedCommand.ExecuteExternalAsync(default(TInjectedRequest));
+        }
+
+        public static async Task<TInjectedResult> ExecuteAsync<TRequest, TResult, TInjectedRequest, TInjectedResult>
+               (this IAsyncAspCommand<TRequest, TResult> command, IAsyncAspCommand<TInjectedRequest, TInjectedResult> injectedCommand, TInjectedRequest request)
+        { 
+            return await injectedCommand.ExecuteExternalAsync(request);
+        }
+
+        public static TCommand Command<TCommand>(this IAsyncCommand command) where TCommand : IAsyncCommand
+        {
+            return Scope.ServiceProvider.GetService<TCommand>();
+        }
+
+    }
+}
