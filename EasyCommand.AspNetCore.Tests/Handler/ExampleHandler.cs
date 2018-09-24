a1a6eda5e4c405b0735fac782aa3a7e3f802c19b
Alex Shannon <alexs1305@gmail.com>
2018-09-24 10:50:56 +0100

    refactor(ExampleHandler): implement new interface design

--- EasyCommand.AspNetCore.Tests/Handler/ExampleHandler.cs
+++ EasyCommand.AspNetCore.Tests/Handler/ExampleHandler.cs
@@ -1,12 +1,19 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace EasyCommand.AspNetCore.Tests.Handler
 {
     class ExampleHandler : IAsyncAspCommandHandler
     {
-        public Task<TResult> ExecuteCommand<TRequest, TResult>(IAsyncAspCommand<TRequest, TResult> command, TRequest request)
+        public Task AfterExecutionAsync<TResponse>(Type command, TResponse response)
         {
-            return Task.FromResult(default(TResult));
+            return Task.CompletedTask;
         }
+
+        public Task BeforeExecutionAsync<TRequest>(Type command, TRequest request)
+        {
+            return Task.CompletedTask;
+        }
+
     }
 }
