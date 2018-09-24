fe0a8208d5acdc7ce55d21d612442ef396ccf13d
Alex Shannon <alexs1305@gmail.com>
2018-09-24 10:50:13 +0100

    refactor(controllers): remove reference to test object

--- EasyCommand.AspNetCore.Tests/Commands/ExampleCommandNested.cs
+++ EasyCommand.AspNetCore.Tests/Commands/ExampleCommandNested.cs
@@ -4,11 +4,8 @@ namespace EasyCommand.AspNetCore.Tests.Commands
 {
     class ExampleCommandNested : AsyncAspCommandNoRequestNoResult
     {
-        private TestObject testObject;
-
-        public ExampleCommandNested(TestObject testObject = null)
+        public ExampleCommandNested()
         {
-            if (!(testObject is null)) this.testObject = testObject;
         }
 
         protected override Task<EmptyCommandResult> ExecuteCommandAsync(EmptyCommandRequest request)
--- EasyCommand.AspNetCore.Tests/Commands/ExampleCommandWithNoRequestOrResult.cs
+++ EasyCommand.AspNetCore.Tests/Commands/ExampleCommandWithNoRequestOrResult.cs
@@ -4,16 +4,13 @@ namespace EasyCommand.AspNetCore.Tests.Commands
 {
     class ExampleCommandWithNoRequestOrResult : AsyncAspCommandNoRequestNoResult
     {
-        private TestObject testObject;
 
-        public ExampleCommandWithNoRequestOrResult(TestObject testObject = null)
+        public ExampleCommandWithNoRequestOrResult()
         {
-            if(!(testObject is null)) this.testObject = testObject;
         }
            
         protected override Task<EmptyCommandResult> ExecuteCommandAsync(EmptyCommandRequest request)
         {
-            testObject.Called();
             return Task.FromResult(new EmptyCommandResult());
         }
     }
